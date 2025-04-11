c8c69809407ba157ba496866f0a4e70e4a102d58
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-04-11 02:09:42 -0300

    ordenando

--- src/manuscript_cover_letter/about.py
+++ src/manuscript_cover_letter/about.py
@@ -1,6 +1,6 @@
 # about.py
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __package__ = "manuscript_cover_letter"
 __program_name__ = "manuscript-cover-letter"
 __author__ = "Fernando Pujaico Rivera" 
